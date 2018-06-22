7d06f53f56e769b7894e978a8db54606e45c0476
assnctr <trollplacez@gmail.com>
2018-06-23 01:49:45 +0300

    Reworked method

--- src/app/misc/uniq.js
+++ src/app/misc/uniq.js
@@ -1,16 +1,3 @@
-export const uniq = a => {
-    let prims = {
-            "boolean": {},
-            "number": {},
-            "string": {}
-        },
-        objs = [];
-
-    return a.filter((item) => {
-        let type = typeof item;
-        if (type in prims)
-            return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
-        else
-            return objs.indexOf(item) >= 0 ? false : objs.push(item);
-    });
+export const uniq = array => {
+    return [...new Set([...array])];
 }
