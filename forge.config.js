f0b7e74c37dd2b72de225a6187786c47a48f4882
assnctr <trollplacez@gmail.com>
2018-06-23 01:46:03 +0300

    Added custom config for building

---
+++ forge.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    "make_targets": {
+        "win32": [
+            "squirrel"
+        ]
+    },
+    "publish_targets": {
+        "win32": ["github"]
+    },
+    "github_repository": {
+        "owner": "assnctr",
+        "name": "unfx-proxy-checker",
+        "draft": false
+    },
+    "electronPackagerConfig": {
+        "name": "unfx-proxy-checker-v1.0.3",
+        "packageManager": "yarn",
+        "appCopyright": "© 2018 ost UNFORCEPROXY (openproxy.space) by ASSNCTR",
+        "icon": "./public/icons/logo.png",
+        "asar": true
+    }
+}
